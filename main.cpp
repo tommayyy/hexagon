1f48653490cb8f68d7826fe8338585e3f12e8f8f
Thomas Kesberg <tom@base.localdomain>
2019-02-05 01:36:37 +0100

    ignore

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <set>
 
+using std::cout;
+using std::endl;
+
 bool is_unique(std::vector<int> *X)
 {
     std::set<int> Y(X->begin(), X->end());
@@ -40,15 +43,64 @@ void o(std::vector<int> *vec)
     std::cout << std::endl;
 }
 
+std::vector<std::vector<int>> v3;
+std::vector<std::vector<int>> v4;
+std::vector<std::vector<int>> v5;
+
+bool search(std::vector<int> result, int step)
+{
+    if (step == 0)
+    {
+        for (auto v : v5)
+        {
+            if (search(v, 1)) {
+                return true;
+            }
+        }
+        return false;
+    } else if (step == 1) {
+
+    }
+
+    return false;
+}
+
 int main()
 {
-    std::vector<int> *v = new std::vector<int>();
-    v->push_back(0);
-    v->push_back(0);
-    v->push_back(0);
-    
-    incc(v);
+    std::vector<int> v;
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(0);
 
-    o(v);
+    cout << "computing vectors... ";
+    while (v.size() < 6)
+    {
+        incc(&v);
+        if (v.size() == 3)
+            v3.push_back(v);
+        if (v.size() == 4)
+            v4.push_back(v);
+        if (v.size() == 5)
+            v5.push_back(v);
+    }
+    cout << "done" << endl;
+
+    cout << "Size: " << v3.size() << endl;
+    o(&v3.back());
+    cout << "Size: " << v4.size() << endl;
+    o(&v4.back());
+    cout << "Size: " << v5.size() << endl;
+    o(&v5.back());
+
+    std::vector<int> result;
+    if (search(result, 0))
+    {
+        cout << "found vector" << endl;
+    }
+    else
+    {
+        cout << "no match" << endl;
+    }
+
     return 0;
 }
